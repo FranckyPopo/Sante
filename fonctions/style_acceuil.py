3001a609de948f02456c8a6da487c120904c4675
FranckyPoop <afrifranck2003@gmail.com>
2022-01-24 21:43:17 +0000

    Fin de la création de la premier fenêtre

--- fonctions/style_acceuil.py
+++ fonctions/style_acceuil.py
@@ -2,5 +2,33 @@
 style_bouton_accueil = {
     "bg": "#D8E9A8",
     "fg": "#191A19",
-    "font": ("Roboto", 18, "bold")
+    "font": ("Roboto", 14, "bold")
+}
+
+# Window 1
+grand_titre = {
+    "fg": "#D8E9A8", 
+    'bg': "#4E9F3D",
+    "justify": "left", 
+    "font": ("Roboto", 24, "bold", "underline")
+}
+
+color_label = {
+    "bg": "#4E9F3D",
+    "font": ("Roboto", 14, "bold")
+}
+
+margin_label = {
+    "pady": 15,
+    "sticky": "W"
+}
+
+bnt_valided = {
+    "bg": "#1E5128",
+    "fg": "#191A19",
+    "font": ("Roboto", 12, "bold")
+}
+
+style_enter = {
+    "sticky": "E"
 }
--- main.py
+++ main.py
@@ -7,9 +7,59 @@ window.minsize(720, 480)
 window.title("CLINIQUE POPO")
 window["bg"] = "#4E9F3D"
 
+def window1():
+    root = tkinter.Tk()
+    root.geometry("720x480")
+    root.minsize(720, 480)
+    root.title("Prendre rendez-vous")
+    root["bg"] = "#4E9F3D"
+    
+    container_title = tkinter.Frame(root, bg="#4E9F3D")
+    
+    titre = tkinter.Label(container_title, text="PRENDRE RENDEZ-VOUS", **style_acceuil.grand_titre).pack(pady=35)
+    
+    container_title.pack()
+    
+    container_forms = tkinter.Frame(root, bg="#4E9F3D")
+    
+    # champ 1
+    last_name = tkinter.StringVar()
+    Label_last_name = tkinter.Label(container_forms, text="Nom: ", **style_acceuil.color_label).grid(row=0, column=0, **style_acceuil.margin_label)
+    enter_last_name = tkinter.Entry(container_forms, textvariable=last_name).grid(row=0, column=1, **style_acceuil.style_enter)
+    
+    # champ 2
+    firs_name = tkinter.StringVar()
+    Label_firs_name = tkinter.Label(container_forms, text="Prénom: ", **style_acceuil.color_label).grid(row=1, column=0, **style_acceuil.margin_label)
+    enter_firsname = tkinter.Entry(container_forms, textvariable=firs_name).grid(row=1, column=1, **style_acceuil.style_enter)
+    
+    # champ 3
+    telephone_number = tkinter.IntVar()
+    Label_telephone_number = tkinter.Label(container_forms, text="Télephone: ", **style_acceuil.color_label).grid(row=2, column=0, **style_acceuil.margin_label)
+    enter_telephone_number = tkinter.Entry(container_forms, textvariable=telephone_number).grid(row=2, column=1, **style_acceuil.style_enter)
+    
+    # champ 4
+    email = tkinter.StringVar()
+    Label_email = tkinter.Label(container_forms, text="Email: ", **style_acceuil.color_label).grid(row=3, column=0, **style_acceuil.margin_label)
+    enter_email = tkinter.Entry(container_forms, textvariable=email).grid(row=3, column=1, **style_acceuil.style_enter)
+
+    # champ 5
+    category = tkinter.StringVar()
+    Label_category = tkinter.Label(container_forms, text="Catégorie: ", **style_acceuil.color_label).grid(row=4, column=0, **style_acceuil.margin_label)
+    enter_category = tkinter.Entry(container_forms, textvariable=category).grid(row=4, column=1, **style_acceuil.style_enter)
+    
+    # Bouton valider
+    bnt_validate = tkinter.Button(container_forms, text="Valider", **style_acceuil.bnt_valided).grid(row=5, column=0, sticky="W")
+    container_forms.pack()
+    
+    root.mainloop()
+    
+    
+
+
+
 frame = tkinter.Frame(window, bg="#4E9F3D")
 
-bnt1 = tkinter.Button(frame, text="Prendre rendez-vous", **style_acceuil.style_bouton_accueil).grid(row=0, column=0, padx=20)
+bnt1 = tkinter.Button(frame, text="Prendre rendez-vous", **style_acceuil.style_bouton_accueil, command=window1).grid(row=0, column=0, padx=20)
 bnt2 = tkinter.Button(frame, text="Tarifs", **style_acceuil.style_bouton_accueil).grid(row=0, column=1, padx=20)
 bnt3 = tkinter.Button(frame, text="Nous retrouver", **style_acceuil.style_bouton_accueil).grid(row=0, column=2, padx=20)
 
