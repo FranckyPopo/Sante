44de816e55c76a7f050c8936393bccdc5f1b846a
FranckyPoop <afrifranck2003@gmail.com>
2022-01-26 11:29:59 +0000

    Réorganisation du code principale

--- main.py
+++ main.py
@@ -10,9 +10,14 @@ window["bg"] = "#4E9F3D"
  
 frame = tkinter.Frame(window, bg="#4E9F3D")
 
-bnt1 = tkinter.Button(frame, text="Prendre rendez-vous", **style_acceuil.style_bouton_accueil, command=fonctionnalite.window1).grid(row=0, column=0, padx=20)
-bnt2 = tkinter.Button(frame, text="Tarifs", **style_acceuil.style_bouton_accueil).grid(row=0, column=1, padx=20)
-bnt3 = tkinter.Button(frame, text="Nous retrouver", **style_acceuil.style_bouton_accueil).grid(row=0, column=2, padx=20)
+bnt1 = tkinter.Button(frame, text="Prendre rendez-vous", **style_acceuil.style_bouton_accueil, command=fonctionnalite.window1)
+bnt1.grid(row=0, column=0, padx=20)
+
+bnt2 = tkinter.Button(frame, text="Carnet de santé", **style_acceuil.style_bouton_accueil)
+bnt2.grid(row=0, column=1, padx=20)
+
+bnt3 = tkinter.Button(frame, text="Nous retrouver", **style_acceuil.style_bouton_accueil, command=fonctionnalite.window3)
+bnt3.grid(row=0, column=2, padx=20)
 
 frame.pack(expand="yes")
 
