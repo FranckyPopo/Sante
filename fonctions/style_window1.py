f3e3d5fd64adf31b9f5bb0b1fc91af787a80ada9
FranckyPoop <afrifranck2003@gmail.com>
2022-01-26 11:49:54 +0000

    Création des fichiers qui von contenir les styles de chaque fenêtre

---
+++ fonctions/style_window1.py
@@ -0,0 +1,32 @@
+# Window 1
+grand_titre = {
+    "fg": "#D8E9A8", 
+    'bg': "#4E9F3D",
+    "justify": "left", 
+    "font": ("Roboto", 24, "bold", "underline")
+}
+
+color_label = {
+    "bg": "#4E9F3D",
+    "font": ("Roboto", 14, "bold")
+}
+
+margin_label = {
+    "pady": 15,
+    "sticky": "W"
+}
+
+bnt_valided = {
+    "bg": "#1E5128",
+    "fg": "#191A19",
+    "font": ("Roboto", 12, "bold")
+}
+
+style_enter = {
+    "sticky": "W"
+}
+
+style_calendar = {
+    "sticky": "W",
+    "pady": 2
+}
