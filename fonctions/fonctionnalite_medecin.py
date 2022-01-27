a9a8506d3e510732755fe06c374f8f31e81b0c97
FranckyPoop <afrifranck2003@gmail.com>
2022-01-27 02:54:25 +0000

    Debut de la création de la fonctionnalité voir consultation du jour

---
+++ fonctions/fonctionnalite_medecin.py
@@ -0,0 +1,11 @@
+import tkinter
+
+def consultation():
+    root = tkinter.Toplevel()
+    root.geometry("720x480")
+    root.resizable(width=False, height=False)
+    root.title("Consultation du jour")
+    
+    
+    
+    root.mainloop()
